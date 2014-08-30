cf58e1461a054627827297d1f95314c4c7fd0da1
Michael Bernstein <michael@fandomhome.com>
2014-08-30 09:12:37 -0500

    first working version.

---
+++ main.py
@@ -0,0 +1,20 @@
+"""`main` is the top level module for your Pyramid application."""
+
+# Import the Pyramid Framework
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+#from webob.exc import HTTPNotFound, HTTPInternalServerError
+
+
+def hello_world(request):
+    """Return a friendly greeting."""
+    return Response('Hello World!')
+
+config = Configurator()
+config.add_route('root', '/')
+config.add_view(hello_world, route_name='root')
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+app = config.make_wsgi_app()
