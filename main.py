24036708053d1727df88a8cb096ecae70ca3185f
craigholland <craig_holland@hotmail.com>
2018-07-24 02:09:31 -0700

    API_Registry working

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ config = Configurator()
 
 
 def root_page(request):
-    """Return a friendly greeting."""
+    """Start it up."""
+    # Create API Registry model and announce being online.
     md = models.Model()
+    if not md.get_model_by_name('API_Registry'):
+        db_main.build(API_models.API_Registry)
+        Build()
+
+
     if md.get_model_by_name('API_Registry'):
         result = 'API Registry ready...'
     else:
@@ -22,11 +28,6 @@ def root_page(request):
 
     return Response('Core Modules Running: ' + result)
 
-
-# Create API Registry model and announce being online
-db_main.build(API_models.API_Registry)
-if models.Model().get_model_by_name('API_Registry'):
-    Build()
 
 # Get Routes from Modules
 _routes = [
